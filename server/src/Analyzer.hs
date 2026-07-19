a1e7b00f8ceace8b3c00b1f82f60f73a277ddeb3
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2026-07-19 23:18:54 +0500

    Server: add diagnostics

--- server/src/Analyzer.hs
+++ server/src/Analyzer.hs
@@ -1,7 +1,17 @@
 module Analyzer where
 
 import Control.Monad (foldM)
+import Data.Text (Text)
+import qualified Data.Map as M
 import Syntax
+
+type Env = M.Map Text Contract
+
+buildEnv :: Program -> Env
+buildEnv terms = M.fromList
+  [ (procName p, contract p)
+  | Located _ (TermProcedure p) <- terms
+  ]
 
 stackEffect :: Instruction -> (Int, Int)
 stackEffect (MathOp _) = (2, 1)
@@ -13,46 +23,83 @@ stackEffect (MemOp MemRead) = (1, 1)
 stackEffect (IoOp IoRead) = (0, 1)
 stackEffect (IoOp _) = (1, 0)
 stackEffect (LitNumber _) = (1, 0)
-stackEffect (CallIdentifier _) = (0, 1)
 stackEffect (ExecToken _) = (0, 1)
-stackEffect ExecuteOp = (1, 0)
 stackEffect _ = (0, 0)
 
-data AnalyzeError = AnalyzeError
-  { errRange :: Range,
-    expected :: Int,
-    actual :: Int,
-    instr :: Instruction
-  }
+data AnalyzeError
+  = InstructionUnderflow Range Int Int Instruction
+  | BranchMismatch Range Int Int
+  | LoopMismatch Range Int
+  | ContractMismatch Range Text Int Int
+  | UndefinedProcedure Range Text
 
-analyzeInstruction :: Int -> Located Instruction -> Either AnalyzeError Int
-analyzeInstruction depth (Located loc instruction) =
+analyzeInstruction :: Env -> Int -> Located Instruction -> Either AnalyzeError Int
+analyzeInstruction env depth (Located loc instruction) =
   case instruction of
-    _
-      | isSimple instruction ->
-          let (takes, puts) = stackEffect instruction
-           in if depth < takes
-                then Left $ AnalyzeError loc takes depth instruction
-                else Right $ depth - takes + puts
-    CondExp ifBranch elseBranch -> analyzeCond loc depth ifBranch elseBranch
-    LoopExp body -> Right depth
-    _ -> Right depth
-  where
-    isSimple (CondExp _ _) = False
-    isSimple (LoopExp _) = False
-    isSimple _ = True
+    CondExp ifBranch elseBranch -> analyzeCond env loc depth ifBranch elseBranch
+    LoopExp body -> analyzeLoop env loc depth body
+    CallIdentifier name ->
+        case M.lookup name env of
+            Just (Contract t p) ->
+                if depth < t
+                    then Left $ InstructionUnderflow loc t depth instruction
+                    else Right $ depth - t + p
+            Nothing -> Left $ UndefinedProcedure loc name
 
-analyzeCond :: Range -> Int -> [Located Instruction] -> Maybe [Located Instruction] -> Either AnalyzeError Int
-analyzeCond loc depth ifBranch elseBranch = do
-    let depthAfterIf = depth - 1
-    if depthAfterIf < 0
-        then Left $ AnalyzeError loc 1 depth (CondExp [] Nothing)
-        else do
-            depthIfPath <- foldM analyzeInstruction depthAfterIf ifBranch
+    _ ->
+      let (t, p) = stackEffect instruction
+       in if depth < t
+            then Left $ InstructionUnderflow loc t depth instruction
+            else Right $ depth - t + p
 
-            depthElsePath <- case elseBranch of
-                Just instructions -> foldM analyzeInstruction depthAfterIf instructions
-                Nothing-> Right depthAfterIf
-            if depthIfPath == depthElsePath
-                then Right depthIfPath
-                else Left $ error "If and else branches leave stack in different states"
+analyzeCond :: Env -> Range -> Int -> [Located Instruction] -> Maybe [Located Instruction] -> Either AnalyzeError Int
+analyzeCond env loc depth ifBranch elseBranch = do
+  let depthAfterIf = depth - 1
+  if depthAfterIf < 0
+    then Left $ InstructionUnderflow loc 1 depth (CondExp [] Nothing)
+    else do
+      depthIfPath <- foldM (analyzeInstruction env) depthAfterIf ifBranch
+
+      depthElsePath <- case elseBranch of
+        Just instructions -> foldM (analyzeInstruction env) depthAfterIf instructions
+        Nothing -> Right depthAfterIf
+      if depthIfPath == depthElsePath
+        then Right depthIfPath
+        else Left $ BranchMismatch loc depthIfPath depthElsePath
+
+analyzeLoop :: Env -> Range -> Int -> [Located Instruction] -> Either AnalyzeError Int
+analyzeLoop env loc depth body = do
+  depthAfteIteration <- foldM (analyzeInstruction env) depth body
+  let delta = depthAfteIteration - depth
+  if delta == 1
+    then Right depth
+    else Left $ LoopMismatch loc delta
+
+analyzeProcedure :: Env -> Range -> ProcedureDef -> Either AnalyzeError Int
+analyzeProcedure env loc procedure = do
+  let c = contract procedure
+      t = takes c
+      p = puts c
+      name = procName procedure
+  finalDepth <- foldM (analyzeInstruction env) t (procBody procedure)
+  if finalDepth == p
+    then Right finalDepth
+    else Left $ ContractMismatch loc name p finalDepth
+
+analyzeTerm :: Env -> Int -> Located Term -> Either AnalyzeError Int
+analyzeTerm env depth (Located loc term) = do
+    case term of
+        TermProcedure proc -> do
+            _ <- analyzeProcedure env loc proc
+            Right depth
+        TermInstruction instr -> do
+            analyzeInstruction env depth (Located loc instr)
+        TermVar _ -> do
+            Right depth
+        TermImport _ -> do
+            Right depth
+
+analyzeProgram :: Program -> Either AnalyzeError Int
+analyzeProgram program = do
+    let env = buildEnv program
+    foldM (analyzeTerm env) 0 program
